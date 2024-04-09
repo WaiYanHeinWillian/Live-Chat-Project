36c31e154c965c47cd77cfef3f694b2247165ee1
WaiYanHein <waiyanhein5796.tgi@gmail.com>
2024-04-09 16:40:44 +0630

    make signup and login form with css style

---
+++ src/components/Login.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>Login</h1>
+  <form v-on:submit.prevent="login">
+    
+    <input type="email" placeholder="Email" v-model="email">
+    <input type="password" placeholder="Password" v-model="password">
+    <button>Login</button>
+  </form>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+    setup(){
+        
+        let email=ref("");
+        let password=ref("");
+        let login=()=>{
+            console.log(email.value,password.value)
+        }
+
+        return {email,password,login}
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Signup.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>Sign Up</h1>
+  <form v-on:submit.prevent="signup">
+    <input type="text" placeholder="Display Name" v-model="displayName">
+    <input type="email" placeholder="Email" v-model="email">
+    <input type="password" placeholder="Password" v-model="password">
+    <button>Sign Up</button>
+  </form>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+    setup(){
+        let displayName=ref("");
+        let email=ref("");
+        let password=ref("");
+        let signup=()=>{
+            console.log(displayName.value,email.value,password.value)
+        }
+
+        return {displayName,email,password,signup}
+    }
+}
+</script>
+
+<style>
+
+</style>
