bf3d3444cc33ef54a35c70fb01dfadf3ac1cae2f
WaiYanHein <waiyanhein5796.tgi@gmail.com>
2024-04-11 14:17:47 +0630

    create navbar component and make logout button work

---
+++ src/components/Navbar.vue
@@ -0,0 +1,49 @@
+<template>
+  <nav>
+    <div>
+        <p>Hi display name</p>
+        <p class="email">Logged in as email</p>
+    </div>
+    <button v-on:click="logout">Logout</button>
+  </nav>
+</template>
+
+<script>
+import { ref } from 'vue'
+import { auth } from '../firebase/config'
+export default {
+    setup(){
+        let error=ref(null);
+        let logout=async()=>{
+            try{
+                await auth.signOut();
+                console.log('user logged out');
+            }catch(err){
+                error.value=err.message;
+                console.log(error.value)
+            }
+        }
+    
+    return {logout}
+    }
+}
+</script>
+
+<style>
+    nav{
+        padding: 20px;
+        border-radius: 1px solid #eee;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    nav p{
+        margin: 2px auto;
+        font-size: 16px;
+        color: #444;
+    }
+    nav p.email{
+        font-size: 14px;
+        color: #999;
+    }
+</style>
