2cd79f9cdbd0f0b3b7fd5c8536752ea0100540e4
WaiYanHein <waiyanhein5796.tgi@gmail.com>
2024-04-12 20:19:03 +0630

    adding new message to firestore by useCollection composable

--- src/components/NewChatForm.vue
+++ src/components/NewChatForm.vue
@@ -8,24 +8,28 @@
 import { ref } from 'vue'
 import { timeStamp } from '../firebase/config';
 import getUser from '../composables/getUser';
+import useCollection from '../composables/useCollection';
 export default {
     setup(){
         let {user}=getUser();
 
+        let {error,addDoc}=useCollection('messages');
+
         let message=ref("");
-        let handleSubmit=()=>{
+        let handleSubmit=async()=>{
             let chat={
             message:message.value,
             name:user.value.displayName,
             created_At:timeStamp()
         }
-        console.log(chat);
+        // console.log(chat);
+        await addDoc(chat);
         message.value="";
         };
 
         
 
-        return {message,handleSubmit}
+        return {message,handleSubmit,error}
     }
 }
 </script>
